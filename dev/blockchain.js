9201992fd062e82b63b4249d536e8b49ef4a49e8
Alicia Perry <hydras15@gmail.com>
2021-10-17 16:46:24 -0500

    add genesis block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,7 +3,10 @@ const sha256 = require('sha256');
 function Blockchain() {
 	this.chain = [];
 	this.pendingTransactions = [];
-}
+
+	//this is a genesis block, the first block to be generated
+	this.createNewBlock(100, '0','0');
+};
 
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
@@ -20,12 +23,12 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 	this.chain.push(newBlock);
 
 	return newBlock;
-}
+};
 
 
 Blockchain.prototype.getLastBlock = function() {
 	return this.chain[this.chain.length - 1];
-}
+};
 
 
 Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
@@ -39,14 +42,14 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
 	this.pendingTransactions.push(newTransaction);
 
 	return this.getLastBlock()['index'] + 1;
-}
+};
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
 	
 	const dataAsString = previousBlockHash + nonce.toString + JSON.stringify(currentBlockData);
 	const hash = sha256(dataAsString);
 	return hash;
-}
+};
 
 
 Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
@@ -55,10 +58,10 @@ Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData)
 	while (hash.substring(0, 4) !== '0000') {
 		nonce++;
 		hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-	}
+	};
 
 	return nonce;
-}
+};
 
 
 
--- dev/test.js
+++ dev/test.js
@@ -1,25 +1,4 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-
-const previousBlockHash = 'OINAISDFN09N09ASDNF09N90ASNDF';
-const currentBlockData = [
-	{
-		amount: 101,
-		sender: 'N90ANS90N90ANSDFN',
-		recipient: '90NA90SNDF90ANSDF09N'
-	},
-	{
-		amount: 30,
-		sender: '09ANS09DFNA8SDNF',
-		recipient: 'UIANSIUDFUIABSDUIFB'
-	},
-	{
-		amount: 200,
-		sender: '89ANS89DFN98ASNDF89',
-		recipient: 'AUSDF89ANSD9FNASD'
-	}
-];
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 27470));
+console.log(bitcoin);
