1eef2fc00b4ca29280f538c67a524a2fabf74e0c
Alicia Perry <hydras15@gmail.com>
2021-10-24 09:49:54 -0500

    addint express js

---
+++ dev/untitled.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express(); 
+
+
+
+
+//fetch entire blockchain
+app get('/blockchain', function (req, res) {
+	
+});
+
+
+
+//create a new transaction
+app post('/transaction', function (req, res) {
+	
+});
+
+
+
+//mine a new block
+app get('/mine, function (req, res') {
+	
+});
+
+
+
+
+app listen(3000, () => {
+	console.log('Listening on port 3000...');
+});
