b5d863a88d0d0a81e11832baee023326aef1d42d
Alicia Perry <hydras15@gmail.com>
2021-10-26 08:29:27 -0500

    adding base for http like functions for mine, post, and transaction

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
 
+app.get('/blockchain', function (req, res) {
+  
+});
+
+
+app.post('/transaction',function(req, res) {
+
+});
+
+
+
+app.get('/mine', function(req, res) {
+
+});
+
+ 
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000...');
+});
