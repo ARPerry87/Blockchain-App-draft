9b2f5916e07600f202edf181ae3a395629f0607b
Alicia Perry <hydras15@gmail.com>
2021-11-10 16:47:26 -0600

    adding nodes 1-5

---
+++ dev/networkNode.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+const uuid = require('uuid').v4;
+const port = process.argv[2];
+
+const nodeAddress = uuid().split('-').join('');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/blockchain', function(req, res) {
+	res.send(bitcoin);
+});
+
+app.post('/transaction', function(req, res) {
+  const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+  res.json({ note: "Transaction will be added in block ${blockIndex}."});
+});
+
+app.get('/mine', function(req, res) {
+
+	const lastBlock = bitcoin.getLastBlock();
+	const previousBlockHash = lastBlock['hash'];
+	const currentBlockData = {
+		transactions: bitcoin.pendingTransactions,
+		index: lastBlock['index'] + 1
+	};
+	const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+	const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+	bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+
+	const newBlock = bitcoin.creatNewBlock(nonce, previousBlockHash, blockHash); 
+	res.json({
+		note: "New block mined successfully", 
+		block: newBlock
+	});
+
+});
+
+app.listen(port, () => console.log(`Listening on ${port} ...`));
